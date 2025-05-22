ae236288d8a11151a3c938371f3b1069492e213d
MidnightCrow00 <orsolyaalbert1@gmail.com>
2025-05-22 21:53:27 +0200

    helyre rakas

--- src/main/java/com/example/savsziget/SavSzigetMod.java
+++ src/main/java/com/example/savsziget/SavSzigetMod.java
@@ -11,8 +11,8 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.entity.LivingEntity;
-
-
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 
 
 public class SavSzigetMod implements ModInitializer {
@@ -22,38 +22,45 @@ public class SavSzigetMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		System.out.println("Sav Sziget Mod initialized!");
-//		vizSebez();
+		vizSebez();
 		csonakCraftolas();
 
 
 	}
 
-//	private void vizSebez() {
-//		ServerTickEvents.END_SERVER_TICK.register(server -> {
-//			tickCounter++;
-//			if (tickCounter >= DAMAGE_COOLDOWN) {
-//				tickCounter = 0;
-//
-//				for (ServerWorld world : server.getWorlds()) {
-//					for (LivingEntity entity : world.getEntitiesByClass(LivingEntity.class, entity -> true, entity -> true)) {
-//
-//						if (entity.isTouchingWater()) {
-//							if (entity instanceof ServerPlayerEntity player) {
-//								player.damage((ServerWorld) world,world.getDamageSources().lava(), 4.0F);
-//
-//								BlockPos pos = player.getBlockPos();
-//								if (world.hasRain(pos) && world.isSkyVisible(pos.up())) {
-//									player.damage((ServerWorld) world,world.getDamageSources().lava(), 1.0F);
-//								}
-//							} else {
-//								entity.damage((ServerWorld) world,world.getDamageSources().lava(), 2.0F);
-//							}
-//						}
-//					}
-//				}
-//			}
-//		});
-//	}
+	private void vizSebez() {
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			tickCounter++;
+			if (tickCounter >= DAMAGE_COOLDOWN) {
+				tickCounter = 0;
+
+				// Játékosok sebzése vízben és esőben - nem módosított rész
+				for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+					World world = player.getWorld();
+					BlockPos pos = player.getBlockPos();
+
+					if (player.isTouchingWater()) {
+						player.damage((ServerWorld) world, world.getDamageSources().lava(), 4.0F);
+					}
+
+					if (world.hasRain(pos) && world.isSkyVisible(pos.up())) {
+						player.damage((ServerWorld) world, world.getDamageSources().lava(), 1.0F);
+					}
+				}
+
+
+				for (ServerWorld world : server.getWorlds()) {
+					for (LivingEntity entity : world.getEntitiesByClass(LivingEntity.class,
+							new Box(-30000000, -64, -30000000, 30000000, 320, 30000000),
+							e -> !(e instanceof ServerPlayerEntity))) {
+						if (entity.isTouchingWater()) {
+							entity.damage((ServerWorld) world, world.getDamageSources().lava(), 2.0F);
+						}
+					}
+				}
+			}
+		});
+	}
 
 
 
